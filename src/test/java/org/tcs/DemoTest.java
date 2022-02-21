9a58d934095d4b1b019f8bdf03ca4d4b4ddb4069
developer319 <1204vikas@gmail.com>
2022-02-21 10:17:29 +0530

    DataProvider is implemented in the DemoTest Class

--- src/test/java/org/tcs/DemoTest.java
+++ src/test/java/org/tcs/DemoTest.java
@@ -1,21 +1,25 @@
 package org.tcs;
 
+import io.cucumber.java.hu.Ha;
+import io.cucumber.java.it.Data;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.tcs.PageComponents.MultiTrip;
 import org.tcs.PageComponents.RoundTrip;
 import org.tcs.PageObjects.TravelHomePage;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 
 public class DemoTest {
 
+
    // By sectionElement= By.id("flightSearchContainer");
 
-    @Test
-    public void flightTest(){
+    @Test(dataProvider = "getData")
+    public void flightTest(HashMap<String,String> reservationDetails){
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\Executables\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         TravelHomePage travelHomePage=new TravelHomePage(driver);
@@ -28,15 +32,24 @@ public class DemoTest {
 
       //  travelHomePage.setBookingStrategy(new MultiTrip(driver,sectionElement));
         travelHomePage.setBookingStrategy("multitrip");
+
+        travelHomePage.checkAvail(reservationDetails);
+        //one way , round trip, multi trip - checkAvailibility(origin,destination
+    }
+    @DataProvider
+    public Object[][] getData(){
         HashMap<String,String> reservationDetails=new HashMap<String,String>();
         reservationDetails.put("origin","MAA");
         reservationDetails.put("destination","HYD");
         reservationDetails.put("destination2","DEL");
-        travelHomePage.checkAvail(reservationDetails);
+        HashMap<String,String> reservationDetails1=new HashMap<String,String>();
+        reservationDetails1.put("origin","DEL");
+        reservationDetails1.put("destination","HYD");
+        reservationDetails1.put("destination2","MAA");
+        return new Object[][]
+                {
+                        {reservationDetails},{reservationDetails1}
+        };
 
-
-        //one way , round trip, multi trip - checkAvailibility(origin,destination
-
-
-    }
+    };
 }
